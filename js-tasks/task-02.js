6426eb53a83c5728239ab5bd8976195739b04467
Vladus125 <167063564+Vladus125@users.noreply.github.com>
2026-01-22 20:54:01 +0000

    added js task solutions

--- js-tasks/task-02.js
+++ js-tasks/task-02.js
@@ -1,1 +1,16 @@
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+const stringToArray = (string) => string.split(" ");
 
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+const DNAtoRNA = (dna) => dna.replace(/T/g, "U");
+
+// https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript
+const min = (list) => Math.min(...list);
+const max = (list) => Math.max(...list);
+
+// https://www.codewars.com/kata/544a54fd18b8e06d240005c0/train/javascript
+const min = (arr, toReturn) => {
+  return toReturn === "value"
+    ? Math.min(...arr)
+    : arr.indexOf(Math.min(...arr));
+};
